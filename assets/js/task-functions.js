783dbe36e9c8bb11bc33dcfb0bb772fe29e994ba
B McCool <mccoolcontact@gmail.com>
2022-09-27 20:18:41 -0300

    bs 5 framework

---
+++ assets/js/task-functions.js
@@ -0,0 +1,16 @@
+ $(document).ready(function(){
+  $("task-color-button").click(function(){
+
+$(function () {
+    $('task-color-button').click(function(){
+        if ($(this).hasClass('task-color-button-selected') {
+                $(this).removeClass('task-color-button-selected');
+                //Insert logic if you want a type of optional click/off click code
+            } 
+            else
+            {
+                $(this).addClass('task-color-button-selected');
+                //Insert event handling logic
+            }
+    })
+});
